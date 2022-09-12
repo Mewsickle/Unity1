25c303409738695063004fc0bbe0fa760546f9ea
Mewsickle <106917007+Mewsickle@users.noreply.github.com>
2022-09-12 19:30:31 +0300

    1 домашка
    дизайн 1 лвл
    поковырялся в скриптах игры, добавил возможность регулировать скорость через скрипт.

---
+++ 3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float walkSpeed = 1f; // внесен модификатор скорости с возможностью редактирования из редактора.
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Vector3 movement; // новая переменная
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
+    {
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource> ();
+    }
+
+    void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+                        
+        movement.Set(horizontal, 0f, vertical);
+        m_Movement = Vector3.ClampMagnitude(movement, 1f) * walkSpeed; //фикс диагональной скорости + модификатор скорости.
+                        
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
+        
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop ();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+    }
+
+    void OnAnimatorMove ()
+    {
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation (m_Rotation);
+    }
+}
