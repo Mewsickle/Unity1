0eff2b789800a0ab0c6d56f1026b487521712904
Mewsickle <106917007+Mewsickle@users.noreply.github.com>
2022-10-06 19:42:50 +0300

    nightmare

--- 3D Beginner/Assets/Scripts/TurretScript.cs
+++ 3D Beginner/Assets/Scripts/TurretScript.cs
@@ -23,9 +23,13 @@ using UnityEngine;
 public class TurretScript : MonoBehaviour
 {
     [SerializeField] Transform player;
+    [SerializeField] Transform launchPoint;
     [SerializeField] GameEnding gameEnding;
-    [SerializeField] private Transform _target;
+    [SerializeField] private Transform target;
     [SerializeField] GameObject fireball;
+    [SerializeField] float launchVelocity;
+    [SerializeField] float cooldownTime;
+    private float nextFireTime = 1;
 
     bool m_IsPlayerInRange;
 
@@ -47,21 +51,29 @@ public class TurretScript : MonoBehaviour
 
     void Update()
     {
+        
+
         if (m_IsPlayerInRange)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
+            //Vector3 direction = target.transform.position - this.transform.position;
+            this.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
             Ray ray = new Ray(transform.position, direction);
             RaycastHit raycastHit;
+            
+            
 
-            if (Physics.Raycast(ray, out raycastHit))
-            {
-                if (raycastHit.collider.transform == player)
+                if (Time.time > nextFireTime)
                 {
-                    transform.LookAt(_target);
-                    Instantiate(fireball, new Vector3(112.27f, 6.43f, 45.46f), Quaternion.identity);
+                    //this.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+                    GameObject ball = Instantiate(fireball, launchPoint.position, launchPoint.rotation);
+                    ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, launchVelocity, 0));
+                    Destroy(ball, 2.0f);
+                    nextFireTime = Time.time + cooldownTime;
 
                 }
-            }
+            
         }
     }
 }
